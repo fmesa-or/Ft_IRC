c6b7e8f0fb43c22a2bd49c4d1b17dfc51c5cf5cc
Cmme <152791634+Crmorale@users.noreply.github.com>
2026-07-23 14:14:44 +0200

    Rename methods for clarity in Replies.cpp

--- src/replies/Replies.cpp
+++ src/replies/Replies.cpp
@@ -39,33 +39,61 @@ std::string Replies::privMsg(const Client &sender, const std::string &target, co
     return ":" + sender.getNickname() + "!" + sender.getUsername() + " PRIVMSG " + target + " :" + text + "\r\n";
 }
 
-std::string Replies::notice(const Client &sender, const std::string &target, const std::string &text) {
+std::string Replies::noticeMsg(const Client &sender, const std::string &target, const std::string &text) {
     return ":" + sender.getNickname() + "!" + sender.getUsername() + " NOTICE " + target + " :" + text + "\r\n";
 }
 
-std::string	Replies::cannotSendToChannel(const Client &client, const std::string &channel) {
-	return ":ft_irc 404 " + client.getNickname() + " " + channel + " :Cannot send to channel\r\n";
-}
 
-std::string Replies::part(const Client &client, const std::string &channel) {
+std::string Replies::partMsg(const Client &client, const std::string &channel) {
    return ":" + client.getNickname() + "!" + client.getUsername() + "@localhost PART " + channel + "\r\n";
 }
 
-std::string Replies::partReason(Client &client, const std::string &channel, const std::string &reason) {
+std::string Replies::partReasonMsg(Client &client, const std::string &channel, const std::string &reason) {
     return ":" + client.getNickname() + "!" + client.getUsername() + "@localhost PART " + channel + " :" + reason + "\r\n";
 }
 
-std::string Replies::join(const Client &client, const std::string &channel) {
+std::string Replies::joinMsg(const Client &client, const std::string &channel) {
     return ":" + client.getNickname() + "!" + client.getUsername() + "@localhost JOIN " + channel + "\r\n";
 }
+
+std::string Replies::kickMsg(const Client &client, const std::string &channel, const std::string &target,
+	const std::string &reason) {
+    	return ":" + client.getNickname() + "!" + client.getUsername() + "@localhost KICK " + channel +
+			" " + target + " :" + reason + "\r\n";
+}
+
+std::string Replies::topicMsg(const Client &client, const std::string &channel, const std::string &topic) {
+    return ":" + client.getNickname() + "!" + client.getUsername() + "@localhost TOPIC " + channel + " :" + topic + "\r\n";
+}
+
+std::string Replies::removeTopicMsg(const Client &client, const std::string &channel) {
+    return ":" + client.getNickname() + "!" + client.getUsername() + "@localhost TOPIC " + channel + " :\r\n";
+}
+
+std::string Replies::quitMsg(const Client &client, const std::string &reason) {
+    return ":" + client.getNickname() + "!" + client.getUsername() + "@localhost QUIT :" + reason + "\r\n";
+}
+
+std::string Replies::closingLinkMsg(const Client &client, const std::string &reason) {
+    return ":ft_irc ERROR :Closing Link: " + client.getNickname() + " (" + reason + ")\r\n";
+}
+
+
+
+
+
 
 //CHANNEL PROTOCOL
 std::string Replies::noSuchChannel(const Client &client, const std::string &channel) {
     return ":ft_irc 403 " + client.getNickname() + " " + channel + " :No such channel\r\n";
 }
 
-std::string Replies::badChannelMask(const Client &client, const std::string &cmd) {
-	return ":ft_irc 476 " + client.getNickname() + " " + cmd + " :Bad Channel Mask\r\n";
+std::string	Replies::cannotSendToChannel(const Client &client, const std::string &channel) {
+	return ":ft_irc 404 " + client.getNickname() + " " + channel + " :Cannot send to channel\r\n";
+}
+
+std::string Replies::badChannelMask(const Client &client, const std::string &channel) {
+	return ":ft_irc 476 " + client.getNickname() + " " + channel + " :Bad Channel Mask\r\n";
 }
 
 std::string Replies::noTopicSet(const Client &client, const std::string &channel) {
@@ -82,4 +110,54 @@ std::string Replies::nameReply(const Client &client, const std::string &channel,
 
 std::string Replies::endOfNames(const Client &client, const std::string &channel) {
     return ":ft_irc 366 " + client.getNickname() + " " + channel + " :End of /NAMES list\r\n";
+}
+
+std::string Replies::channelModeIs(const Client &client, const std::string &channel, const std::string &modes) {
+    return ":ft_irc 324 " + client.getNickname() + " " + channel + " " + modes + "\r\n";
+}
+
+std::string Replies::unknownMode(const Client &client, const std::string &mode) {
+    return ":ft_irc 472 " + client.getNickname() + " " + mode + " :is unknown mode char to me\r\n";
+}
+
+std::string Replies::whoReply(const Client &requestor, const std::string &channel, const Client &target,
+	const std::string &prefix) {
+    	return ":ft_irc 352 " + requestor.getNickname() + " " + channel + " " + target.getUsername() + " localhost ft_irc "
+			+ target.getNickname() + " H" + prefix + " :0 " + target.getNickname() + "\r\n";
+}
+
+std::string Replies::endOfWho(const Client &client, const std::string &target) {
+    return ":ft_irc 315 " + client.getNickname() + " " + target + " :End of /WHO list.\r\n";
+}
+
+std::string Replies::notChannelOperator(const Client &client, const std::string &channel) {
+    return ":ft_irc 482 " + client.getNickname() + " " + channel + " :You're not channel operator\r\n";
+}
+
+std::string Replies::userNotInChannel(const Client &client, const std::string &nickname, const std::string &channel) {
+    return ":ft_irc 441 " + client.getNickname() + " " + nickname + " " + channel + " :They aren't on that channel\r\n";
+}
+
+std::string Replies::notOnChannel(const Client &client, const std::string &channel) {
+	return ":ft_irc 442 " + client.getNickname() + " " + channel + " :You're not on that channel\r\n";
+}
+
+std::string	Replies::userOnChannel(const Client &client, const std::string &nickname, const std::string &channel) {
+	return	":ft_irc 443 " + client.getNickname() + " " + nickname + " " + channel + " :is already on channel\r\n";
+}
+
+std::string	Replies::alreadyInvited(const Client &client, const std::string &nickname, const std::string &channel) {
+	return	":ft_irc 443 " + client.getNickname() + " " + nickname + " " + channel + " :is already invited\r\n";
+}
+
+std::string Replies::inviting(const Client &client, const std::string &nickname, const std::string &channel) {
+    return ":ft_irc 341 " + client.getNickname() + " " + nickname + " " + channel + "\r\n";
+}
+
+std::string Replies::invite(const Client &client, const std::string &nickname, const std::string &channel) {
+    return ":" + client.getNickname() + "!" + client.getUsername() + "@localhost INVITE " + nickname + " :" + channel + "\r\n";
+}
+
+std::string Replies::modeNewOperator(const std::string &channel, const std::string &nickname) {
+    return ":ft_irc MODE " + channel + " +o " + nickname + "\r\n";
 }
