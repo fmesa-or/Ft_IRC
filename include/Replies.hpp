61890935688a10e818c0f086374366ab000f42f1
Cmme <152791634+Crmorale@users.noreply.github.com>
2026-07-23 14:11:16 +0200

    Refactor Replies.hpp for message function names

--- include/Replies.hpp
+++ include/Replies.hpp
@@ -27,25 +27,41 @@ public:
 
     static std::string	notRegistered(const Client &client);
 
+	static std::string	whoReply(const Client &requestor, const std::string &channel, const Client &target,
+		const std::string &prefix);
+ 
+	static std::string	endOfWho(const Client &client, const std::string &target);
+
 
 	//Messages
 	static std::string	privMsg(const Client &sender, const std::string &target, const std::string &text);
 
-	static std::string	notice(const Client &sender, const std::string &target, const std::string &text);
+	static std::string	noticeMsg(const Client &sender, const std::string &target, const std::string &text);
 
-	static std::string	cannotSendToChannel(const Client &client, const std::string &channel);
+	static std::string	partMsg(const Client &client, const std::string &channel);
 
-	static std::string	part(const Client &client, const std::string &channel);
+	static std::string	partReasonMsg(Client &client, const std::string &channel, const std::string &reason);
 
-	static std::string	partReason(Client &client, const std::string &channel, const std::string &reason);
+	static std::string	joinMsg(const Client &client, const std::string &channel);
 
-	static std::string	join(const Client &client, const std::string &channel);
+	static std::string	kickMsg(const Client &client, const std::string &channel, const std::string &target,
+		const std::string &reason);
+
+	static std::string	topicMsg(const Client &client, const std::string &channel, const std::string &topic);
+
+	static std::string	removeTopicMsg(const Client &client, const std::string &channel);
+
+	static std::string	quitMsg(const Client &client, const std::string &reason);
+
+	static std::string Replies::closingLinkMsg(const Client &client, const std::string &reason);
 
 
 	//Channel protocol msg
 	static std::string	noSuchChannel(const Client &client, const std::string &channel);
 
-	static std::string	badChannelMask(const Client &client, const std::string &cmd);
+	static std::string	cannotSendToChannel(const Client &client, const std::string &channel);
+
+	static std::string	badChannelMask(const Client &client, const std::string &channel);
 
 	static std::string	noTopicSet(const Client &client, const std::string &channel);
 
@@ -54,6 +70,26 @@ public:
 	static std::string	nameReply(const Client &client, const std::string &channel, const std::string &names);
 
 	static std::string	endOfNames(const Client &client, const std::string &channel);
+
+	static std::string	channelModeIs(const Client &client, const std::string &channel, const std::string &modes);
+
+	static std::string	unknownMode(const Client &client, const std::string &mode);
+
+	static std::string	notChannelOperator(const Client &client, const std::string &channel);
+
+	static std::string	userNotInChannel(const Client &client, const std::string &nickname, const std::string &channel);
+
+	static std::string	notOnChannel(const Client &client, const std::string &channel);
+
+	static std::string	userOnChannel(const Client &client, const std::string &nickname, const std::string &channel);
+	
+	static std::string	alreadyInvited(const Client &client, const std::string &nickname, const std::string &channel);
+
+	static std::string	inviting(const Client &client, const std::string &nickname, const std::string &channel);
+
+	static std::string	invite(const Client &client, const std::string &nickname, const std::string &channel);
+
+	static std::string	modeNewOperator(const std::string &channel, const std::string &nickname);
 };
 
 #endif
